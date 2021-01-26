aef39b4b802eef2a174f73e5c48f3f8ef7b25785
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-01-26 09:17:48 -0400

    Added Cube to start messing with component code

---
+++ Assets/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    void Start()
+    {
+        //Vector3 position;
+        //position.x = 0.0f;
+        //position.y = 2.0f;
+        //position.z = 0.0f;
+        //gameObject.transform.position = position;
+
+        //gameObject.transform.position = new Vector3(0, 2, 0); // implicit type casting/conversion (from int to float)
+
+        gameObject.transform.eulerAngles = new Vector3(0.0f, 45.0f, 0.0f);
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
